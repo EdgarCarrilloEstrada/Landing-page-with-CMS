8ed93bbbb5a07050ef0eabb19d3fa39ba107d037
Edgar Carrillo Estrada <edgar.carrillo.e@gmail.com>
2026-04-25 23:14:38 -0600

    Postgres integration
    - Created and modified files to use postgresql as production database.
    
    - Updated packages
    
    - Rearranged dockerfile and created docker-compose file
    
    - .env.example file created

--- grupolias/grupolias/settings/production.py
+++ grupolias/grupolias/settings/production.py
@@ -1,21 +1,31 @@
 from .base import *
+import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['164.92.112.192', 'grupolias.com', 'localhost', 'www.grupolias.com'] 
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "grupolias.com,www.grupolias.com").split(",")
 
-SECRET_KEY = 'z-he-j@ji!8)@qfif0-(0c8@!qr$bz_nxjixrc3an370k-xy#a'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'paginaweb',
-        'USER': 'usuariopagina',
-        'PASSWORD': 'GrupoLIASPagina', 
-        'HOST': '127.0.0.1',
-        'PORT': '5001',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "db"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
+
+# Seguridad básica para producción
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Wagtail
+WAGTAILADMIN_BASE_URL = os.getenv("BASE_URL", "https://grupolias.com")
 
 LOGGING = {
     'version': 1,
@@ -35,3 +45,43 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+
+# from .base import *
+
+# DEBUG = False
+
+# ALLOWED_HOSTS = ['164.92.112.192', 'grupolias.com', 'localhost', 'www.grupolias.com']
+
+# SECRET_KEY = 'z-he-j@ji!8)@qfif0-(0c8@!qr$bz_nxjixrc3an370k-xy#a'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'paginaweb',
+#         'USER': 'usuariopagina',
+#         'PASSWORD': 'GrupoLIASPagina',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5001',
+#     }
+# }
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'root': {
+#         'handlers': ['console'],
+#         'level': 'WARNING',
+#     },
+# }
+
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
