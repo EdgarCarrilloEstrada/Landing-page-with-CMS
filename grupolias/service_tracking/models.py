cf2cb0f9b29df1f47c1142ba16d653c0cd82bc54
devMini <edgar.danmm@gmail.com>
2026-05-08 15:59:19 -0600

    Update(service-tracking): add verbose name to each field to display it on spanish and merge request date and request time to handle it as a datetime field

--- grupolias/service_tracking/models.py
+++ grupolias/service_tracking/models.py
@@ -9,46 +9,46 @@ from shared.enums.payment_status import PaymentStatus
 class ServiceRequest(models.Model):
 
     # Información general
-    client_name = models.CharField(max_length=255)
-    service_type = models.CharField(max_length=50, choices=Services.choices)
-    service_description = models.TextField()
+    client_name = models.CharField(max_length=255, verbose_name="Nombre del cliente")
+    service_type = models.CharField(max_length=50, choices=Services.choices, verbose_name="Tipo de servicio")
+    service_description = models.TextField(verbose_name="Tema a realizar o resolver")
 
     # Personal involucrado
-    technician_name = models.CharField(max_length=255)
-    operator_responsible = models.CharField(max_length=255)
-    assigned_by = models.CharField(max_length=255)  # arquitecto o ingeniero
+    technician_name = models.CharField(max_length=255, verbose_name="Nombre del técnico")
+    operator_responsible = models.CharField(max_length=255, verbose_name="Responsable del seguimiento")
+    assigned_by = models.CharField(max_length=255, verbose_name="Asignado por")  # arquitecto o ingeniero
 
     # Fechas y horas
-    request_date = models.DateField()
-    request_time = models.TimeField()
+    request_datetime = models.DateTimeField(verbose_name="Fecha y hora de solicitud")
 
-    execution_date = models.DateField(null=True)
-    completion_date = models.DateField(null=True)
+    execution_date = models.DateField(null=True, verbose_name="Fecha de realización")
+    completion_date = models.DateField(null=True, verbose_name="Fecha de termino")
 
-    payment_date = models.DateField(null=True)
+    payment_date = models.DateField(null=True, verbose_name="Fecha de pago")
 
     # Ubicación
-    place_name = models.CharField(max_length=255)
-    place_manager_name = models.CharField(max_length=255)
-    address = models.TextField()
-    state = models.CharField(max_length=5, choices=States.choices)
+    place_name = models.CharField(max_length=255, verbose_name="Nombre del lugar")
+    place_manager_name = models.CharField(max_length=255, verbose_name="Nombre del encargado del lugar")
+    address = models.TextField(verbose_name="Dirección")
+    state = models.CharField(max_length=5, choices=States.choices, verbose_name="Estado")
 
     # Resultados del servicio
-    work_description = models.TextField()
+    work_description = models.TextField(verbose_name="Descripción de lo que se realizó")
 
     # Costos
-    technician_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    company_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    technician_cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Costo del técnico")
+    company_cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Costo de Grupo LIAS")
 
     # Pago
-    payment_status = models.CharField(max_length=20, choices=PaymentStatus.choices, default=PaymentStatus.PENDING)
+    payment_status = models.CharField(max_length=20, choices=PaymentStatus.choices, default=PaymentStatus.PENDING, verbose_name="Estado del pago")
 
     # Extras
-    general_observations = models.TextField(blank=True)
+    general_observations = models.TextField(blank=True, verbose_name="Observaciones")
 
     # Control interno
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"{self.client_name} - {self.service_type} - {self.request_date}"
