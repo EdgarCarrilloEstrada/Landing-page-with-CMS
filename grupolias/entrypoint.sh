414f338942225ae2a363eb0f8f1b76a46a396f76
devMini <edgar.danmm@gmail.com>
2026-06-28 12:34:15 -0600

    Update(config): update docker config for production environment

--- grupolias/entrypoint.sh
+++ grupolias/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+set -e
+
+echo "Setting permissions..."
+
+mkdir -p /app/static
+mkdir -p /app/media
+
+chown -R wagtail:wagtail /app/static
+chown -R wagtail:wagtail /app/media
+
+echo "Running migrations..."
+python manage.py migrate --noinput
 
 # collect all static files to the root directory
 python manage.py collectstatic --no-input
 
 # start the gunicorn worker processws at the defined port
-gunicorn grupolias.wsgi:application --bind 0.0.0.0:8000 &
-
-wait
+exec gunicorn grupolias.wsgi:application --bind 0.0.0.0:8000
