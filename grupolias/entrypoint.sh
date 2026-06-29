7cf40899b6db69e8df51b87acf584b75c08c5283
devMini <edgar.danmm@gmail.com>
2026-06-29 16:23:07 -0600

    Revert "Delete(config): do not apply migrations on  containers init"
    This reverts commit 281ae04836f922f06b7f1576b88b61e962c1cb9a.

--- grupolias/entrypoint.sh
+++ grupolias/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+
 # collect all static files to the root directory
 python manage.py collectstatic --no-input
 
